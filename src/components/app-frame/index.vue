cff772b6a2cf89f2c0ed7f14228deeab80b4a48a
lunjj@create80.com <lunjj@create80.com>
2019-12-21 12:02:04 +0800

    add dark mode

---
+++ src/components/app-frame/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <v-app-bar
+        :src="darkFlag ? 'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg' : ''"
+        app
+        color="top_bar"
+        flat
+    >
+      <div class="d-flex align-center">
+        <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="../../../public/img/app/frame/frame-logo.png"
+            transition="scale-transition"
+            width="32"
+        />
+        <h2>GYM</h2>
+      </div>
+      <v-spacer></v-spacer>
+      <v-btn
+          icon
+          text
+      >
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+      <v-btn
+          text
+          icon
+          @click="changeMode"
+      >
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+    </v-content>
+    <v-bottom-navigation
+        fixed
+        :value="activeBtn"
+        grow
+        color="teal"
+    >
+      <v-btn>
+        <span>Recents</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Favorites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Nearby</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
+</template>
+<script lang="ts">
+  import {Component, Vue} from "vue-property-decorator";
+
+  @Component
+  export default class AppFrame extends Vue {
+    private activeBtn: number = 1;
+    private darkFlag: boolean = this.$vuetify.theme.dark;
+
+    changeMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    }
+  }
+</script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -8,7 +8,20 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: '#ee44aa',
+        top_bar: '#fff',
+        content_bg: '#000',
+        primary: '#1976D2',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+      },
+      dark: {
+        top_bar: '#282828',
+        content_bg: '#212121',
+        primary: '#1976D2',
         secondary: '#424242',
         accent: '#82B1FF',
         error: '#FF5252',
@@ -17,5 +30,6 @@ export default new Vuetify({
         warning: '#FFC107',
       },
     },
+    // dark: true,
   },
 });
