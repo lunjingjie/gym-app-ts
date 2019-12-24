9c3cff2f171e71dffdd3d1ee3f14c129677af1fb
lunjj@create80.com <lunjj@create80.com>
2019-12-24 15:21:24 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,5 @@
 module.exports = {
   "lintOnSave": false,
-  /*"pwa": {
-    "name": "CSDN",
-    "themeColor": "#4DBA87",
-    "msTileColor": "#000000",
-    "appleMobileWebAppCapable": "yes",
-    "appleMobileWebAppStatusBarStyle": "black",
-    "workboxPluginMode": "InjectManifest",
-    "workboxOptions": {
-      "swSrc": "./public/registerServiceWorker.ts",
-      "importWorkboxFrom": "disabled",
-      "exclude": [
-        {},
-        {},
-        {}
-      ]
-    }
-  },*/
   "transpileDependencies": [
     "vuetify"
   ]
